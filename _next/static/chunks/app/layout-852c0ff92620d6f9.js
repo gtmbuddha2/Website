99a5fb639e7cc07a4661707a3abfbebb37194e35
gtmbuddha <nimesgautam@gmail.com>
2023-06-02 14:29:47 +0545

    nextjs website

---
+++ _next/static/chunks/app/layout-852c0ff92620d6f9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{5055:function(n,e,u){Promise.resolve().then(u.t.bind(u,8392,23))},8392:function(){}},function(n){n.O(0,[667,488,744],function(){return n(n.s=5055)}),_N_E=n.O()}]);
